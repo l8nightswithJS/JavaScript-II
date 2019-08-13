0eea5d6c0b1a2043695ecaa958d8b2d48ecd5f1d
eddie jimenez <ejmrtears89@gmail.com>
2019-08-12 18:52:29 -0700

    comitting arrays assignments and callbacks assignments

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,22 +26,46 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  // getLength pas9ses the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length) {
+  console.log(length)
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  var lastItem = arr[arr.length - 1]
+  return cb(lastItem);
 }
+last(items, function(lastItem){
+  console.log(lastItem);
+})
 
 function sumNums(x, y, cb) {
+  return cb(x + y);
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+sumNums(5, 8, function(add){
+  console.log(add);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
-
-function contains(item, list, cb) {
+multiplyNums(5, 8, function(multiply){
+  console.log(multiply);
+})
+function contains(items, list, cb) {
+  if (list === items) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
+  contains("gum", items, function(trueFalse){
+    console.log(trueFalse);
+  })
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
